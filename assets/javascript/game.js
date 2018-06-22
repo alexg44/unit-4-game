dd95ce79f3d3d915fc23949286de96acb455d38a
alexg44 <alex.gshon@gmail.com>
2018-06-21 23:44:11 -0700

    java replacement

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    var target = (Math.random());
+    var result = "result";
+    var yourScore = 0;
+
+    $("#computerPick").append("<h2>Target: </h2>" + target + "<br /> <br /> <br />"
+
+}
+
+var crystal1 = 0;
+
+$(#crystal_1).onclick(function() {
+    crystal1  = i;
+    i = i + 3
+})
+crystal1
+console.log(crystal_1);
